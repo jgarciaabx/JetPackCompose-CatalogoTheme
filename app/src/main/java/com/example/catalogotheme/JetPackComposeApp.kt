ebe9931bd66f9b8cfa19f721a5b3d8bc98945777
jgarcia <jgarcia.abx@gmail.com>
2023-01-15 11:26:59 -0500

    Login con Dagger Hilt

---
+++ app/src/main/java/com/example/catalogotheme/JetPackComposeApp.kt
@@ -0,0 +1,8 @@
+package com.example.catalogotheme
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class JetPackComposeApp :Application() {
+}
--- app/src/main/java/com/example/catalogotheme/login/data/network/LoginService.kt
+++ app/src/main/java/com/example/catalogotheme/login/data/network/LoginService.kt
@@ -1,17 +1,17 @@
 package com.example.catalogotheme.login.data.network
 
-import android.util.Log
-import com.example.catalogotheme.core.network.RetrofitHelper
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
 import retrofit2.create
+import javax.inject.Inject
 
-class LoginService {
-    private val retrofit = RetrofitHelper.getRetrofit()
+class LoginService @Inject constructor(private val loginClient: LoginClient){
+//    private val retrofit = RetrofitHelper.getRetrofit()
 
     suspend fun doLogin(user:String, password:String):Boolean{
         return withContext(Dispatchers.IO){
-            val response = retrofit.create(LoginClient::class.java).doLogin()
+            val response = loginClient.doLogin()
             response.body()?.success ?: false
         }
     }
